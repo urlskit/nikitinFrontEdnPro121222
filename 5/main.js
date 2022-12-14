614b7bb2a43c3fd2687a10573d4cd57e4baa610e
Urlskit <urlskit@gmail.com>
2022-12-14 02:25:31 +0200

    Комит 5-ой домашки

---
+++ 5/main.js
@@ -0,0 +1,8 @@
+const firstNumber = parseInt(prompt('Введите первое число'));
+const secondNumber = parseInt(prompt('Введите второе число'));
+const thirdNumber = parseInt(prompt('Введите третье число'));
+const sAr = 3;
+
+const sum = (firstNumber + secondNumber + thirdNumber) / sAr;
+
+alert(`Cреднее арифметическое = ${sum}`);
